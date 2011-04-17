1ac7fa6a8f54a4a910d1ca21fa46e4d82baa9b28
Ivan Acosta-Rubio <ivan@bakedweb.net>
2011-04-17 18:47:37 -0500

    bunch of stuff

--- lib/card.rb
+++ lib/card.rb
@@ -7,7 +7,7 @@ class Card
                  :jack, :queen, :king, :ace]
 
   def initialize(suit, value)
-    raise unless VALID_SUITS.include?(suit)
+    raise "Invalid Card" unless VALID_SUITS.include?(suit)
     @suit = suit
     @value = value
   end
@@ -22,5 +22,80 @@ class Card
   def self.valid_values
     VALID_VALUES
   end
+end
+
+class Deck
+
+  attr_reader :stack
+
+  def initialize
+    @valid_suits = Card.valid_suits
+    @valid_values = Card.valid_values
+    @stack = Array.new
+
+    @valid_suits.each do |suit|
+      @valid_values.each{|value | @stack << Card.new(suit, value)}
+    end
+  end
+
+  def size
+    @stack.size
+  end
+
+  def cards
+    @stack
+  end
+end
+
+class Player
+  attr_reader :cards
+
+  def initialize
+    @cards = Array.new
+  end
+
+  def cards
+    @cards
+  end
+end
+
+class  Hand
+  def initialize
+    @cards = Array.new
+  end
+  def cards
+#    raise unless self.cards.size > 5
+    @cards
+  end
+end
+
+class TexasHoldem
+end
+
+class Dealer
+  def initialize game
+    raise "Unsupported Game" unless game.class == TexasHoldem
+    @game = game
+    @deck = Deck.new
+    @hand = Hand.new
+  end
+
+  def flop
+    3.times{ |n| deal }
+  end
+
+  def hand
+    @hand
+  end
+
+  def deck
+    @deck
+  end
+
+  def deal
+    @hand.cards << @deck.cards.pop
+  end
+
+  alias :river :deal ; alias  :turn :deal
 
 end
--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -2,7 +2,25 @@ require 'rubygems'
 require 'rspec'
 require 'lib/card'
 
+  def pair
+    [ Card.new(:suit, :one),
+      Card.new(:heart, :one),
+      Card.new(:heart, :ten),
+      Card.new(:heart, :two),
+      Card.new(:heart, :five)]
+  end
+
+  def single
+    [ Card.new(:suit, :ace),
+      Card.new(:heart, :one),
+      Card.new(:heart, :ten),
+      Card.new(:heart, :two),
+      Card.new(:heart, :five)]
+  end
+
+
 describe Card, "suit" do
+
   VALID_SUITS = Card.valid_suits
 
   VALID_SUITS.each do |suit|
@@ -27,5 +45,87 @@ describe Card, "values" do
       c.value.should == value
     end
   end
+end
 
+describe Deck do
+  it "has the products of the suits and values" do
+    d = Deck.new
+    d.size.should == Card.valid_suits.size * Card.valid_values.size
+  end
+end
+
+describe Player do
+
+  before(:all) do
+    @player = Player.new
+  end
+
+  subject{  @player }
+
+  it{ subject.cards.size.should == 0 }
+
+  (1..5).each do |number|
+    it "can have #{number} card" do
+      subject.cards << Card.new(:hearts, number.to_sym)
+    end
+  end
+
+end
+
+describe Hand , "Texas Holdem" do
+  it "only knows about the texas holden " do
+    f = Hand.new
+  end
+
+  it "raises if more than 6 cards" do
+    @hand = Hand.new
+
+    def add_cards
+      6.times do |number|
+        @hand.cards << Card.new(:hearts, number.to_sym)
+      end
+    end
+
+    pending
+    lambda{ add_cards }.should raise_error
+  end
+end
+
+
+describe Dealer, "knows about texas holdem" do
+
+  it "requires a game type" do
+    lambda{ Dealer.new }.should raise_error
+  end
+
+  subject  { Dealer.new(TexasHoldem.new) }
+
+  it "have an stack of cards" do
+    subject.deck.class == Deck.new
+  end
+
+  it "knows about the hand" do
+    subject.hand.class == Hand.new
+  end
+
+  it "flops 3 cards" do
+    subject.flop
+    subject.hand.cards.size.should == 3
+  end
+
+  it "knows about the turn " do
+    subject.flop
+    subject.turn
+    subject.hand.cards.size.should == 4
+  end
+
+  it "knows about the river" do
+    subject.flop
+    subject.turn
+    subject.river
+    subject.hand.cards.size.should == 5
+  end
+end
+
+describe TexasHoldem do
 end
