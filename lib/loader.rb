97f63a08a3c955e91aa1dc68a1705f6bc2f57185
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-11-12 19:27:50 -0600

    Quickly building a Game.

--- lib/loader.rb
+++ lib/loader.rb
@@ -6,3 +6,13 @@ require './lib/judge'
 require './lib/player'
 require './lib/truco'
 require './lib/deck'
+
+def build_game
+  @player1 = Player.new
+  @player2 = Player.new
+  @game = Truco.new [@player1, @player2]
+  @game.deal_to_players
+  #@game.to_s
+  @game.play
+  nil
+end
